e0b8d9aaa5385fa67dcd30880567f3956ea2e22c
nicholas <nickptran@gmail.com>
2022-10-09 16:21:17 -0700

    fix again?

--- R/modules/database.R
+++ R/modules/database.R
@@ -1,4 +1,23 @@
-source("R/modules/database_credentials.R")
+library(RPostgres)
+library(DBI)
+library(pool)
+library(tidyverse)
 
 
+pool <- dbPool(Postgres(),
+  dbname = "dakj5goln6tg15",
+  host = "ec2-44-207-133-100.compute-1.amazonaws.com",
+  port = 5432,
+  user = "drpevzrnxesrae",
+  password = "e3f84705089a803f65456ceae142a0579246aa21d5d78c5bd466ce0fee970f92"
+)
+
+onStop(function(){
+  poolClose(pool)
+})
+
+
+variables <- pool %>%
+  tbl("variables") %>% 
+  collect()
 
--- server.R
+++ server.R
@@ -2,6 +2,9 @@ library(tidyverse)
 source("R/functions/data_tables.R")
 source("R/modules/database.R")
 
-server <- function(input, output, session) {
 
+
+
+server <- function(input, output, session) {
+  output$variableTable <- dt1(variables)
 }
