a08330ee4db4741b229361998a678692201935e3
nicholas <nickptran@gmail.com>
2022-10-19 17:33:57 -0700

    test

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,10 @@ FROM ghcr.io/virtualstaticvoid/heroku-docker-r:shiny
 
 RUN apt-get update && apt-get install -y --no-install-recommends \ 
   sudo \ 
-  libpq-dev
+  libpq-dev \ 
+  libcurl4-openssl-dev 
+
+RUN install.r RPostgres
 
 ENV PORT=8080
 
--- init.R
+++ init.R
@@ -7,17 +7,3 @@ helpers.installPackages(
   "uuid",
   "DBI"
 )
-
-# packages <- c(
-#   "shiny",
-#   "tidyverse",
-#   "DT",
-# )
-
-# install_if_missing <- function(x) {
-#   if(x %in% rownames(installed.packages()) == FALSE) {
-#     install.packages(p, clean = TRUE, quiet = TRUE)
-#   }
-# }
-
-# invisible(sapply(packages, install_if_missing()))
