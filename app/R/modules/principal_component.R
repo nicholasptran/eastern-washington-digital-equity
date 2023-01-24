aa9e6f30d7c7c78e542fc31ba715165c4bee010e
nicholas <nickptran@gmail.com>
2023-01-24 14:49:40 -0800

    added notebook with pc

--- app/R/modules/principal_component.R
+++ app/R/modules/principal_component.R
@@ -1,6 +1,10 @@
 library(tidyverse)
+library(psych)
+
 
 data <- read.csv("app/data/combined_data.csv")
+
+
 # str(data)
 standardized_data <- read.csv("app/data/z_score_data.csv")
 standardized_data <- standardized_data[3:27]
@@ -36,24 +40,26 @@ round(num_pc, 2)
 # insert the pc data into a variable
 pc_data <- prcomp(standardized_data, scale = FALSE)
 
-# inverse the vectors
-pc_data$rotation <- -pc_data$rotation
-
-# inverse the pc data
-# pc_data$x <- -pc_data$x
-
-data[1, 3:27]
-
-pc_data$rotation[, 1]
-
-# pc 1 -4 
-pc_data$x[, 1:4]
-
-pc_data$sdev
 
 # square the stdev to turn into eigen value
 eigen_value <- pc_data$sdev^2
 eigen_value
+
+# inverse the vectors
+pc_data$rotation <- -pc_data$rotation
+
+
+
+# inverse the pc data
+# pc_data$x <- -pc_data$x
+
+
+# vectors
+pc_data$rotation[, 1:4]
+
+# pc 1 -4
+pc_data$x[, 1:4]
+
 
 
 pc_data
